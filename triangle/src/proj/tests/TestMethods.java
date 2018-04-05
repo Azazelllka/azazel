83010f7058bbbb46d3fbc6f4fe539b592df4de2c
Margo <devil.azazel@ya.ru>
2018-04-06 01:47:34 +0500

    Deleted last 3 tests

--- triangle/src/proj/tests/TestMethods.java
+++ triangle/src/proj/tests/TestMethods.java
@@ -26,35 +26,5 @@ public class TestMethods {
 		if (!isTrue2) Assert.fail();
 		
 	}
-	
-	@Test
-	public void isEquilateralTest() {
-		
-		MyTriangle testTriangleProps = new MyTriangle();
-		
-		String s = testTriangleProps.isEquilateral(6, 4, 6);
-		Assert.assertEquals(s, "Треугольник равнобедренный");
-		
-	}
-	
-	@Test
-	public void trianglePerimeterTest() {
-		
-		MyTriangle testTriangleProps = new MyTriangle();
-		
-		int perimeter = testTriangleProps.trianglePerimeter(6, 4, 6);
-		Assert.assertEquals(perimeter, 16);
-		
-	}
-	
-	@Test
-	public void triangleSquareTest() {
-		
-		MyTriangle testTriangleProps = new MyTriangle();
-		
-		String square = testTriangleProps.triangleSquare(7, 9, 12);
-		Assert.assertEquals(square, "422,79");
-		
-	}
 
 }
